853f8781ad4d22d06e7ec57274633a49ae2d9ccd
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2024-02-28 15:37:24 +0300

    ready to code

---
+++ Login/Screens/CreateAccountScreen.swift
@@ -0,0 +1,29 @@
+//
+//  CreateAccountScreen.swift
+//  Login
+//
+//  Created by Özgün Can Beydili on 28.02.2024.
+//
+
+import UIKit
+
+class CreateAccountScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Login/ViewModels/LoginScreenViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginScreenViewModel.swift
+//  Login
+//
+//  Created by Özgün Can Beydili on 28.02.2024.
+//
+
+import Foundation
